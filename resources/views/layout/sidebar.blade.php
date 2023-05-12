8b167608d379afa471304bcb42c939634d74f4d3
RheinaYulia <rheinayulia27@gmail.com>
2023-05-12 07:33:43 +0700

    first commit

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,77 @@
+<!-- Sidebar -->
+<div id="content">
+<div class="sidebar">
+			
+			<div class="sidebar-background"></div>
+			<div class="sidebar-wrapper scrollbar-inner">
+				<div class="sidebar-content">
+					<ul class="nav">
+						<li class="nav-item active">
+							<a href="/">
+								<i class="fas fa-home"></i>
+								<p>Dashboard</p>
+								<span class="badge badge-count">5</span>
+							</a>
+						</li>
+						<li class="nav-section">
+							<span class="sidebar-mini-icon">
+								<i class="fa fa-ellipsis-h"></i>
+							</span>
+							<h4 class="text-section">Components</h4>
+						</li>
+						<li class="nav-item">
+							<a data-toggle="collapse" href="#base">
+								<i class="fas fa-layer-group"></i>
+								<p>Data Barang</p>
+								<span class="caret"></span>
+							</a>
+							<div class="collapse" id="base">
+								<ul class="nav nav-collapse">
+									<!-- <li>
+										<a href="index.html">
+											<span class="fas fa-home"> Barang Masuk</span>
+										</a>
+									</li> -->
+									<li>
+										<a href="tambahBrg">
+											<span class="sub-item">Tambah Data Barang</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</li>
+
+						<li class="nav-item">
+							<a data-toggle="collapse" href="#Laporan">
+								<i class="fas fa-layer-group"></i>
+								<p>Laporan</p>
+								<span class="caret"></span>
+							</a>
+							<div class="collapse" id="Laporan">
+								<ul class="nav nav-collapse">
+									<!-- <li>
+										<a href="index.html">
+											<span class="fas fa-home"> Barang Masuk</span>
+										</a>
+									</li> -->
+									<li>
+										<a href="DataBrgMasuk">
+											<span class="sub-item">Data Barang Masuk</span>
+										</a>
+									</li>
+									<li>
+										<a href="DataBrgKeluar">
+											<span class="sub-item">Data Barang Keluar</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</li>
+
+						
+					</ul>
+				</div>
+			</div>
+		</div>
+</div>
+		<!-- End Sidebar -->
